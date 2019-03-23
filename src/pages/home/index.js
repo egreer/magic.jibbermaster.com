66c919cfb00bf871a9ff82b648088427fd0d1dde
Eric Greer <eric@apsis.io>
2019-03-23 16:18:37 -0400

    adding missing react helment from home

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { Helmet } from "react-helmet";
+import { Helmet } from "react-helmet-async";
 import { NavLink } from "react-router-dom";
 import { Button } from "reactstrap";
 
