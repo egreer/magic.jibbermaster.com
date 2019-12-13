5c48be19d332f9992f7d223fc7d47ec7b6aa0e13
Eric Greer <eric@apsis.io>
2019-12-13 10:11:35 -0500

    Updates

---
+++ precache-manifest.89674ed820d3ef85ee7cc06c4c4c4ac8.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cb03501cbc376edfda715e0cb9995dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bde6b05b098d934f8dfa",
+    "url": "/static/css/2.6937eb24.chunk.css"
+  },
+  {
+    "revision": "f4d0ab8ca22bb2ff4075",
+    "url": "/static/css/main.47816d87.chunk.css"
+  },
+  {
+    "revision": "bde6b05b098d934f8dfa",
+    "url": "/static/js/2.92fc6401.chunk.js"
+  },
+  {
+    "revision": "fe2f43a278cf9be632912037b15131b3",
+    "url": "/static/js/2.92fc6401.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f4d0ab8ca22bb2ff4075",
+    "url": "/static/js/main.b4c1197e.chunk.js"
+  },
+  {
+    "revision": "10707d5ad73b821f7225",
+    "url": "/static/js/runtime-main.bf1ff018.js"
+  },
+  {
+    "revision": "2c00b5d48c86e01dc9e3592d8605b2c1",
+    "url": "/static/media/archenemy-back.2c00b5d4.jpg"
+  },
+  {
+    "revision": "c7f5000711206fd903fd723f90f4e945",
+    "url": "/static/media/planechase-back.c7f50007.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aef2327cc072500bd44aac3bd4fdfff9.js"
+  "/precache-manifest.89674ed820d3ef85ee7cc06c4c4c4ac8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
