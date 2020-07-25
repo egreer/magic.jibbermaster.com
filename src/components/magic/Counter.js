3feb6cfde7ee127febdddf29f932332a415bb479
Eric Greer <eric@apsis.io>
2020-07-25 12:05:03 -0400

    update counters to use hooks

--- src/components/magic/Counter.js
+++ src/components/magic/Counter.js
@@ -1,32 +1,21 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import { Button } from "reactstrap";
 import { getCounterType } from "../../mtg/card.js";
-export class Counter extends Component {
-  state = {
-    counter: 0
-  };
 
-  incrementCounter = () => {
-    const { counter } = this.state;
-    this.setState({ counter: counter + 1 });
-  };
-
-  render() {
-    const { card } = this.props;
-    const { counter } = this.state;
-    const type = getCounterType(card);
-    return (
-      <div>
-        <h1 className="text-shadow noselect">{counter}</h1>
-        <Button
-          onClick={this.incrementCounter}
-          color="dark"
-          size="lg"
-          className="btn-translucent"
-        >
-          <h3 className="mb-0">Add {type} Counter</h3>
-        </Button>
-      </div>
-    );
-  }
-}
+export const Counter = ({ card }) => {
+  const [counter, setCounter] = useState(0);
+  const type = getCounterType(card);
+  return (
+    <div>
+      <h1 className="text-shadow noselect">{counter}</h1>
+      <Button
+        onClick={() => setCounter(counter + 1)}
+        color="dark"
+        size="lg"
+        className="btn-translucent"
+      >
+        <h3 className="mb-0">Add {type} Counter</h3>
+      </Button>
+    </div>
+  );
+};
