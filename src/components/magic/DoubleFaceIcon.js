66b488087859ac5ea6cad177d164ee1bbc22e9a2
Eric Greer <eric@apsis.io>
2021-05-07 18:24:17 -0500

    update Double Faced icon

--- src/components/magic/DoubleFaceIcon.js
+++ src/components/magic/DoubleFaceIcon.js
@@ -1,5 +1,4 @@
 import React from "react";
-import merge from "lodash/merge";
 import cn from "classnames";
 
 export const DoubleFaceIcon = ({
@@ -8,20 +7,16 @@ export const DoubleFaceIcon = ({
   invert = false,
   style = {}
 }) => {
-  const backdropStyle = backdrop ? { backgroundColor: "#ffffff" } : {};
+  const classes = {
+    "ms-dfc-day": enabled,
+    "ms-dfc-night": !enabled,
+    "text-light": enabled && invert,
+    "text-dark": !enabled && invert,
+    "bg-white": backdrop
+  };
   return (
     <span>
-      {enabled ? (
-        <i
-          className={cn("ms ms-dfc ms-dfc-day", { "text-light": invert })}
-          style={merge({}, backdropStyle, style)}
-        />
-      ) : (
-        <i
-          className={cn("ms ms-dfc ms-dfc-night", { "text-dark": invert })}
-          style={merge({}, backdropStyle, style)}
-        />
-      )}
+      <i className={cn("ms ms-dfc", classes)} style={style} />
     </span>
   );
 };
