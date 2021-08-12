1b568cf61b4247a24868c1fed085ed16aa7a3a86
Eric Greer <eric@apsis.io>
2021-08-11 23:14:56 -0400

    adding counter icons

--- src/components/magic/Counter.js
+++ src/components/magic/Counter.js
@@ -2,6 +2,30 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import { getCounterType } from "../../mtg/card.js";
 
+export const CounterIcon = ({ type, className = "", style = {} }) => {
+  const icon = (ms, verticalAlign = "initial") => (
+    <i
+      className={`ms mm-fw ${ms} ${className}`}
+      style={{ verticalAlign, ...style }}
+    ></i>
+  );
+
+  switch (type) {
+    case "Doom":
+      return icon("ms-counter-doom");
+    case "Scroll":
+      return icon("ms-counter-lore", "bottom");
+    case "Charge":
+      return icon("ms-counter-charge");
+    case "Pressure":
+      return icon("ms-counter-mining");
+    case "Flame":
+      return icon("ms-counter-flame");
+    default:
+      return null;
+  }
+};
+
 export const Counter = ({ card }) => {
   const [counter, setCounter] = useState(card?.counter || 0);
   const type = getCounterType(card);
@@ -11,7 +35,9 @@ export const Counter = ({ card }) => {
   };
   return (
     <div>
-      <h1 className="text-shadow noselect">{counter}</h1>
+      <h1 className="text-shadow noselect">
+        {counter} <CounterIcon type={type} />
+      </h1>
       <Button
         onClick={incrementCounter}
         variant="dark"
